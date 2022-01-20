8c43c8099623d1e8fe5879a1fbe4d71687431b27
JosueMosquera <82737793+JosueMosquera@users.noreply.github.com>
2022-01-20 12:43:17 -0500

    myChanges

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,30 @@ import 'package:qr_reader/pages/mapa_page.dart';
 
 import 'package:qr_reader/providers/scan_list_provider.dart';
 import 'package:qr_reader/providers/ui_provider.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => new UiProvider() ),
-        ChangeNotifierProvider(create: (_) => new ScanListProvider() ),
+        ChangeNotifierProvider(create: (_) => new UiProvider()),
+        ChangeNotifierProvider(create: (_) => new ScanListProvider()),
       ],
-
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'QR Reader',
         initialRoute: 'home',
         routes: {
-          'home': ( _ ) => HomePage(),
-          'mapa': ( _ ) => MapaPage(),
+          'home': (_) => HomePage(),
+          'mapa': (_) => MapaPage(),
         },
         theme: ThemeData(
-          primaryColor: Colors.deepPurple,
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Colors.deepPurple
-          )
-        ),
+            primaryColor: Colors.deepPurple,
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+                backgroundColor: Colors.blueGrey)),
       ),
     );
-
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,16 +4,13 @@ import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/direcciones_page.dart';
 import 'package:qr_reader/pages/mapas_page.dart';
 
-import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
 import 'package:qr_reader/providers/ui_provider.dart';
 
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
-
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,54 +19,45 @@ class HomePage extends StatelessWidget {
         title: Text('Historial'),
         actions: [
           IconButton(
-            icon: Icon( Icons.delete_forever ), 
-            onPressed: (){
-
-              Provider.of<ScanListProvider>(context, listen: false)
-                .borrarTodos();
-
-            }
-          )
+              icon: Icon(Icons.delete_forever),
+              onPressed: () {
+                Provider.of<ScanListProvider>(context, listen: false)
+                    .borrarTodos();
+              })
         ],
       ),
       body: _HomePageBody(),
-
-     bottomNavigationBar: CustomNavigationBar(),
-     floatingActionButton: ScanButton(),
-     floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-   );
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
 
-
 class _HomePageBody extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-
     // Obtener el selected menu opt
     final uiProvider = Provider.of<UiProvider>(context);
-    
+
     // Cambiar para mostrar la pagina respectiva
     final currentIndex = uiProvider.selectedMenuOpt;
 
     // Usar el ScanListProvider
-    final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: false);
 
-    switch( currentIndex ) {
-
+    switch (currentIndex) {
       case 0:
         scanListProvider.cargarScanPorTipo('geo');
         return MapasPage();
 
-      case 1: 
+      case 1:
         scanListProvider.cargarScanPorTipo('http');
         return DireccionesPage();
 
       default:
         return MapasPage();
     }
-
-
   }
 }
